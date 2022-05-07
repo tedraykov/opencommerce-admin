71fc618f2e27f0a70cb718ab12a585e27deb0b40
tedraykov <tedraykov@gmail.com>
2022-05-08 01:57:09 +0300

    ci: update package version

--- release.config.js
+++ release.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     branches: ['trunk'],
     plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        ["@semantic-release/git", {
+            "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }],
         ['@codedependant/semantic-release-docker', {
             dockerTags: ['{{version}}'],
             dockerArgs: {
